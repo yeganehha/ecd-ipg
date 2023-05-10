d227bca0cd5514d391f9014d63d7fd6c5395720b
erfan ebrahimi <me@erfanebrahimi.ir>
2023-05-10 09:18:53 +0330

    set additional data

--- src/Ecd.php
+++ src/Ecd.php
@@ -6,18 +6,55 @@ abstract class Ecd
 {
     protected $key;
     protected $terminal_number;
+    protected $language  ;
+    protected $date  ;
+    protected $time  ;
 
-    public function __construct($terminal_number = null , $key = null)
+    protected $amount ;
+    protected $convertRate ;
+
+    /**
+     * @param string|int $terminal_number
+     * @param string $key
+     * @param boolean $baseOnRial
+     * @param string $language
+     * @param string $timeZone
+     * @throws \Exception
+     */
+    public function __construct($terminal_number = null , $key = null , $baseOnRial = true,  $language = "fa" , $timeZone = "Asia/Tehran")
     {
         if ( $terminal_number )
             $this->setTerminalNumber($terminal_number);
         if ( $key )
             $this->setKey($key);
+        if ( $language == "fa" )
+            $this->persian();
+        else
+            $this->english();
+
+        $date = new \DateTime("now", new \DateTimeZone($timeZone));
+        $this->time = $date->format("h:i");
+        $this->date = $date->format("Y-m-d");
+
+        if ( $baseOnRial )
+            $this->baseOnRial();
+        else
+            $this->baseOnToman();
     }
 
-    public static function instance($terminal_number = null , $key = null)
+    /**
+     * Generate instance of class for call statically
+     * @param string|int $terminal_number
+     * @param string $key
+     * @param boolean $baseOnRial
+     * @param string $language
+     * @param string $timeZone
+     * @return $this
+     * @throws \Exception
+     */
+    public static function instance($terminal_number = null , $key = null , $baseOnRial = true, $language = "fa" , $timeZone = "Asia/Tehran")
     {
-        return new static($terminal_number , $key);
+        return new static($terminal_number  , $key  , $baseOnRial ,  $language , $timeZone );
     }
     /**
      * Key get from IPG
@@ -39,6 +76,66 @@ abstract class Ecd
     public function setTerminalNumber($terminal_number)
     {
         $this->terminal_number = $terminal_number;
+        return $this;
+    }
+
+    /**
+     * Change language of gateway to english
+     * @return $this
+     */
+    public function english()
+    {
+        $this->language = "en";
+        return $this;
+    }
+
+    /**
+     * Change language of gateway to persian
+     * @return $this
+     */
+    public function persian()
+    {
+        $this->language = "fa";
+        return $this;
+    }
+
+
+    /**
+     * call when your system currency is Rial.
+     * @return $this
+     */
+    public function baseOnRial()
+    {
+        $this->convertRate = 1 ;
+        return $this;
+    }
+
+    /**
+     * call when your system currency is Toman.
+     * @return $this
+     */
+    public function baseOnToman()
+    {
+        $this->convertRate = 10 ;
+        return $this;
+    }
+
+
+    /**
+     * set amount of transaction
+     * @param int $amount
+     * @param boolean|null $baseOnRial
+     * @return $this
+     */
+    public function setAmount($amount, $baseOnRial = null)
+    {
+
+        if ( $baseOnRial === true)
+            $this->baseOnRial();
+        elseif ( $baseOnRial === false )
+            $this->baseOnToman();
+
+        $this->amount = $amount  * $this->convertRate;
         return $this;
     }
 }
